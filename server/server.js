e3ec24f76af884578a17b152d83cafc8ace89083
zephyr45 <anuraaagsingh.45@gmail.com>
2025-07-01 09:57:00 +0530

    Update API URLs and feature image logic

--- server/server.js
+++ server/server.js
@@ -30,7 +30,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173", // for local development
+      "https://ecommerce-client-337k.onrender.com" // your deployed frontend
+    ],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
